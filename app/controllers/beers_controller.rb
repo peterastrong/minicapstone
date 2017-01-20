6be8a5aaefaee3e2bc0662211c3427d3828d7eac
Peter Strong <peter.strong@gmail.com>
2017-01-19 21:32:14 -0500

    start working with rest

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,4 +3,20 @@ class BeersController < ApplicationController
     @all_beers = Beer.all 
     render "all_beers.html.erb"
   end 
+
+  def new_beer_method
+    render "new_beer.html.erb"
+  end 
+
+  def new_beer_result_method
+    @beer = Beer.new(brand: params[:brand], style: params[:style], name: params[:name], package_size: params[:package_size], bottle_size: params[:bottle_size], rating_1to99: params[:rating], price: params[:price])
+    @beer.save
+    render "new_beer_result.html.erb"
+  end
+
+  def index
+    @beers = Beer.all? 
+    render "index.html.erb"
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/all_beers', to: 'beers#all_beers_method'
+
+  get "/new_beer", to: "beers#new_beer_method"
+  post "/new_beer_result", to: "beers#new_beer_result_method"
+
+  get "/beers", to: "beers#index"
 end
