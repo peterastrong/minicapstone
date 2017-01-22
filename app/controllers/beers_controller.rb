fed3bf1b827de683e3d20a12bdb8fdf79e409d59
Peter Strong <peter.strong@gmail.com>
2017-01-22 10:28:46 -0500

    learn to make all restful

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,19 +1,5 @@
 class BeersController < ApplicationController
-  def all_beers_method
-    @all_beers = Beer.all 
-    render "all_beers.html.erb"
-  end 
-
-  def new_beer_method
-    render "new_beer.html.erb"
-  end 
-
-  def new_beer_result_method
-    @beer = Beer.new(brand: params[:brand], style: params[:style], name: params[:name], package_size: params[:package_size], bottle_size: params[:bottle_size], rating_1to99: params[:rating], price: params[:price])
-    @beer.save
-    render "new_beer_result.html.erb"
-  end
-
+  
   def index
     @beers = Beer.all 
     render "index.html.erb"
@@ -24,4 +10,13 @@ class BeersController < ApplicationController
     render "show.html.erb"
   end 
 
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @beer = Beer.new(brand: params[:brand], style: params[:style], name: params[:name], package_size: params[:package_size], bottle_size: params[:bottle_size], rating_1to99: params[:rating], price: params[:price])
+    @beer.save
+    render "create.html.erb"
+  end 
 end
