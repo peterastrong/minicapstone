637a8282c4d621964991ed051de4a02f44e4c34e
Peter Strong <peter.strong@gmail.com>
2017-01-23 21:27:43 -0500

    make more restful and add flash messages

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,7 +1,7 @@
 class BeersController < ApplicationController
   
   def index
-    @beers = Beer.all 
+    @beers = Beer.all.sort 
     render "index.html.erb"
   end 
 
@@ -17,6 +17,29 @@ class BeersController < ApplicationController
   def create
     @beer = Beer.new(brand: params[:brand], style: params[:style], name: params[:name], package_size: params[:package_size], bottle_size: params[:bottle_size], rating_1to99: params[:rating], price: params[:price])
     @beer.save
-    render "create.html.erb"
+    flash[:success] = "Congrats for adding another beer to the list!!"
+    redirect_to "/beers/#{@beer.id}"
   end 
+
+  def edit 
+    @beer = Beer.find_by(id: params[:id])
+    render "edit.html.erb"
+  end 
+
+  def update
+    @beer = Beer.find_by(id: params[:id])
+    @beer.assign_attributes(brand: params[:brand], style: params[:style], name: params[:name], package_size: params[:package_size], bottle_size: params[:bottle_size], rating_1to99: params[:rating], price: params[:price])
+    @beer.save
+    flash[:success] = "The beer has been updated."
+    redirect_to "/beers/#{@beer.id}"
+  end
+
+  def destroy
+    @beer = Beer.find_by(id: params[:id])
+    @beer_name_display = @beer.name
+    @beer.destroy
+    flash[:danger] = "You have successfully deleted this beer."
+    redirect_to "/beers"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to:"beers#index" 
 
   get "/beers", to: "beers#index"
-  get "beers/new", to: "beers#new"
-  post "beers", to: "beers#create"
+  get "/beers/new", to: "beers#new"
+  post "/beers", to: "beers#create"
   get "/beers/:id", to: "beers#show"
+  get "/beers/:id/edit", to: "beers#edit"
+  patch "/beers/:id", to: "beers#update"
+  delete "/beers/:id", to: "beers#destroy"
 end
